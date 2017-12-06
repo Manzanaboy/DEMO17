3a4da506c510342d933fa4db825ef5edc84d65a4
chevre sama <damireptil@gmail.com>
2017-12-06 23:41:22 +0100

    Update projet17.c

--- projet17.c
+++ projet17.c
@@ -1,6 +1,6 @@
 /**
  * \file		289148.c
- * \version		1.0
+ * \version		2.2
  * \date		2017-11-02
  * \author		Perez Grether Damian 289148
  * \brief		Programme pour le projet du cours CS-111(c)
@@ -75,6 +75,12 @@ static void erreur_indice_ligne_colonne(int indice, int indicePersonne);
  */
 static void erreur_superposition(int indiceP_A, int indiceP_B);
 
+static int lireVrebose();
+static int lireGrille(int verbose);
+static int lireSim(int verbose);
+static int lireMonde(int verbose);
+static int lirenbp(int verbose, int n);
+static void placerPers(int grandTab[][NB_INFO_PERS], int verbose,  int n, int nbp);
 static void print(int tab[], int a);
 static int rebouclement(int x,int n);
 static void grille(int tab[][NB_INFO_PERS], int n, int nbp);
@@ -82,7 +88,10 @@ static int libre(int tab[][NB_INFO_PERS], int nbp, int x, int y);
 static void nouvBut(int n, int tab[][NB_INFO_PERS], int i);
 static void move(int n, int tab[][NB_INFO_PERS], int nbp, int i);
 static void contamine(int tab[][NB_INFO_PERS], int i, int nbp, int n);
+static void mergemoi(int tab[],int liun[], int lide[], int tun, int tde);
+static void msort(int tab[], int l);
 static int simul(int tab[][NB_INFO_PERS], int n, int nbp, int afG, int nbvacc);
+static void regroupe(int tab[][NB_INFO_PERS], int tailleRes, int n, int nbp, int afG, int nbSim);
 // ############################ END ##################################
 
 // *******************************************************************
@@ -96,108 +105,124 @@ static bool	verbose;
 
 int main(void)
 {
-
-	int reponse, nbSim, n, nbp, afG;
-	
-	//verbose
-	scanf("%d", &reponse);
-	if(reponse==0){
-		verbose=0;
-	}
-	else{
-		verbose=1;
-	}
-	
-	//affichage de la grille
-	if (verbose){
-		printf("afficher la grille ? 0=non 1=oui\n");
-	}
-	scanf("%d", &afG);
-	
-	//nombre de simulations
-	if (verbose){
-		printf("nombre de simulations : \n");
-	}
-	scanf("%d",&nbSim);
-	if (nbSim <= 0){
-		erreur_nbSim(nbSim);
-		return 0;
-	}
-	if (verbose){
-		printf("vous avez choisis %d simulation(s)\n", nbSim);
-	}
-	
-	//taille du monde
-	 if (verbose){
-		 printf("choisissez la taille du monde\n");
-	}
-	scanf("%d", &n);
-	if (n < T_MIN_MONDE){
-		erreur_taille_monde(n);
-		return 0;
-	}
-	if (verbose){
-		printf("le monde fait %d*%d \n", n, n);
-	}
-	
-	//nombre de personnes
-	if (verbose){
-		printf("combien de personnes habitent ce monde ?\n");
-	}
-	scanf("%d", &nbp);
-	if (nbp < NB_MIN_PERS || nbp>(n*n) ){
-		erreur_nbP(nbp,n);
-		return 0;
-	}
-	else{
-		if (verbose){
-		printf("il y a %d personnes dans ce monde\n",nbp);
-		}
-	}
-	
-	//test des personnes
+	//rendu 1
+	int nbSim, n, nbp, afG;
+	verbose = lireVrebose();
+	afG = lireGrille(verbose);
+	nbSim = lireSim(verbose);
+	n = lireMonde(verbose);
+	nbp = lirenbp(verbose, n);
 	int grandTab[nbp][NB_INFO_PERS];
-	int a, b, k;
-	if (verbose){
-		printf("placer les gens\n");
-		}
-	//placement pers 1
-	scanf("%d %d %d %d", &grandTab[0][LIGNEPERS], &grandTab[0][COLPERS], &grandTab[0][LIGNEBUT], &grandTab[0][COLBUT]);
-	grandTab[0][ETAT] = CONT;
-	//placement autres pers
-	for (k=1;k<nbp;k++){
-		scanf("%d %d %d %d", &grandTab[k][LIGNEPERS], &grandTab[k][COLPERS], &grandTab[k][LIGNEBUT], &grandTab[k][COLBUT]);
-		grandTab[k][ETAT] = NORMAL;
-	//test indices
-		for (b=LIGNEPERS; b<COLBUT; b++){
-			if (grandTab[k][b] < 0 || grandTab[k][b] > n-1){
-				erreur_indice_ligne_colonne(grandTab[k][b],k);
-				return 0;
-			}
-		}
-	}
-	//test superposition
-	for (a=1; a<nbp; a++){
-		for (k=0; k<a; k++){
-			if (grandTab[k][LIGNEPERS] == grandTab[a][LIGNEPERS] && grandTab[k][COLPERS] == grandTab[a][COLPERS]){
-				erreur_superposition(k, a);
-				return 0;
-			}
-		}
-	}
-	int deuxiemeTab[nbp][NB_INFO_PERS];
+	placerPers(grandTab, verbose, n, nbp);
 	
-	simul(grandTab, n, nbp, afG, 0);
-	for(int i=0;i<nbp;i++){
-		print(grandTab[i],NB_INFO_PERS);
+	//simulations
+	int tailleRes=0;
+	for (int k=nbp; k>1; k--){ 
+		tailleRes = tailleRes + (k-1);
 		}
-	
+	regroupe(grandTab, tailleRes, n, nbp, afG, nbSim);
 	return EXIT_SUCCESS;
 }
 
 // *******************************************************************
 //              ***        mes fonctions        ***
 // *******************************************************************
+static int lireVrebose(){
+	int reponse;
+	scanf("%d", &reponse);
+	if(reponse==0){
+		return 0;
+	}
+	else{
+		return 1;
+	}
+	}
+
+static int lireGrille(int verbose){
+	int afG;
+	if (verbose){
+		printf("#afficher la grille ? 0=non 1=oui\n");
+	}
+	scanf("%d", &afG);
+	return afG;
+	}
+
+static int lireSim(int verbose){
+	int nbSim;
+	if (verbose){
+		printf("#nombre de simulations : \n");
+	}
+	scanf("%d",&nbSim);
+	if (nbSim <= 0){
+		erreur_nbSim(nbSim);
+		exit(EXIT_FAILURE);
+	}
+	if (verbose){
+		printf("#vous avez choisis %d simulation(s)\n", nbSim);
+	}
+	return nbSim;
+	}
+
+static int lireMonde(int verbose){
+	int n;
+	if (verbose){
+		 printf("#choisissez la taille du monde\n");
+	}
+	scanf("%d", &n);
+	if (n < T_MIN_MONDE){
+		erreur_taille_monde(n);
+		exit(EXIT_FAILURE);
+	}
+	if (verbose){
+		printf("#le monde fait %d*%d \n", n, n);
+	}
+	return n;
+	}
+
+static int lirenbp(int verbose, int n){
+	int nbp;
+	if (verbose){
+		printf("#combien de personnes habitent ce monde ?\n");
+	}
+	scanf("%d", &nbp);
+	if (nbp < NB_MIN_PERS || nbp>(n*n) ){
+		erreur_nbP(nbp,n);
+		exit(EXIT_FAILURE);
+	}
+	else{
+		if (verbose){
+		printf("#il y a %d personnes dans ce monde\n",nbp);
+		}
+	}
+	return nbp;
+	}
+
+static void placerPers(int grandTab[][NB_INFO_PERS], int verbose,  int n, int nbp){
+	int a, b, k;
+	if (verbose){
+		printf("#placer les gens\n");
+		}
+	scanf("%d %d %d %d", &grandTab[0][LIGNEPERS], &grandTab[0][COLPERS], &grandTab[0][LIGNEBUT], &grandTab[0][COLBUT]);
+	grandTab[0][ETAT] = CONT;
+	for (k=1;k<nbp;k++){
+		scanf("%d %d %d %d", &grandTab[k][LIGNEPERS], &grandTab[k][COLPERS], &grandTab[k][LIGNEBUT], &grandTab[k][COLBUT]);
+		grandTab[k][ETAT] = NORMAL;
+		for (b=LIGNEPERS; b<COLBUT; b++){
+			if (grandTab[k][b] < 0 || grandTab[k][b] > n-1){
+				erreur_indice_ligne_colonne(grandTab[k][b],k);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	for (a=1; a<nbp; a++){
+		for (k=0; k<a; k++){
+			if (grandTab[k][LIGNEPERS] == grandTab[a][LIGNEPERS] && grandTab[k][COLPERS] == grandTab[a][COLPERS]){
+				erreur_superposition(k, a);
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+	}
 
 static int rebouclement(int x,int n){
 	if(x < POSMIN || x == n){
@@ -205,16 +230,10 @@ static int rebouclement(int x,int n){
 		}
 		return x;
 	}
-
-/*static int abs(int a){
-	if (a<0){
-		a = -a;
-		}
-	return a;
-	}*/
 	
 static void grille(int tab[][NB_INFO_PERS], int n, int nbp){
 	int i, j;
+	printf("#");
 	for (i=0; i<n; i++){
 		printf(" _");
 		}
@@ -226,20 +245,22 @@ static void grille(int tab[][NB_INFO_PERS], int n, int nbp){
 			}
 		}
 	for (i=0; i<nbp; i++){
-		gr[tab[i][COLPERS]][tab[i][LIGNEPERS]] = tab[i][ETAT];
-		if (gr[tab[i][COLBUT]][tab[i][LIGNEBUT]] == VIDE){
-			gr[tab[i][COLBUT]][tab[i][LIGNEBUT]] = BUT;
+		gr[tab[i][LIGNEPERS]][tab[i][COLPERS]] = tab[i][ETAT];
+		if (gr[tab[i][LIGNEBUT]][tab[i][COLBUT]] == VIDE){
+			gr[tab[i][LIGNEBUT]][tab[i][COLBUT]] = BUT;
 			}
 		}
 	for (i=0; i<n; i++){
+		printf("#");
 		for (j=0; j<n; j++){
 			printf("|");
 			switch (gr[i][j]){
-			
 				case VIDE:
 					printf("_");
 					break;
 				case INCUB:
+					printf("I");
+					break;
 				case NORMAL:
 					printf("N");
 					break;
@@ -248,13 +269,13 @@ static void grille(int tab[][NB_INFO_PERS], int n, int nbp){
 					break;
 				case VACC:
 					printf("V");
+					break;
 				case BUT:
 					printf("B");
 					break;
 			}
 		}
 		printf("|\n");
-		
 		}
 	}
 	
@@ -280,72 +301,35 @@ static void nouvBut(int n, int tab[][NB_INFO_PERS], int i){
 		if (indx == tab[i][LIGNEPERS] && indy == tab[i][COLPERS] ){
 			pos=0;
 			}
-		if (indx == tab[i][LIGNEBUT] && indy == tab[i][COLBUT] ){
-			pos=0;
-			}
 		} while (pos!=1);
 	tab[i][LIGNEBUT]=indx;
 	tab[i][COLBUT]=indy;
 }
 
-static void move(int n, int tab[][NB_INFO_PERS], int nbp, int i){
-	//i = indice de la personne
-	//moving variables
-	int difx = tab[i][LIGNEBUT] - tab [i][LIGNEPERS];       //but - position
-	int depx = tab[i][LIGNEPERS];
-	int dify = tab[i][COLBUT] - tab[i][COLPERS];        //but - position
-	int depy = tab[i][COLPERS];
-	int dep;
-	int k=0;
-	///////////////////////////
+static int shortest(int position, int but, int n){
+	int dif = but - position;
+	int dep=0;
+	if (dif < 0){
+		if (abs(dif) <= (n/2)){
+			dep = GAUCHE;	
+			}
+		else {
+			dep = DROITE;	
+			}
+		}
+	if (dif > 0){
+		if (abs(dif) <= (n/2)){
+			dep = DROITE;
+			}
+		else {
+			dep = GAUCHE;	
+			}
+		}
+	return dep;
+	}
 
-			//but atteint
-	if (tab[i][LIGNEPERS] == tab[i][LIGNEBUT] && tab[i][COLPERS] == tab[i][COLBUT]){
-		nouvBut(n,tab,i);
-		}
-	//but a gauche
-	if (difx < 0){
-		if (abs(difx) <= (n/2)){
-			depx = tab[i][LIGNEPERS]+GAUCHE;	
-			}
-		else {
-			depx = tab[i][LIGNEPERS]+DROITE;	
-			}
-		}
-	
-		//but a droite
-	if (difx > 0){
-		if (abs(difx) <= (n/2)){
-			depx = tab[i][LIGNEPERS]+DROITE;
-			}
-		else {
-			depx = tab[i][LIGNEPERS]+GAUCHE;	
-			}
-		}
-			
-	//but en haut
-	if(dify < 0){
-		if(abs(dify) <= (n/2)){
-			depy = tab[i][COLPERS]+HAUT;
-			}
-		else{
-			depy = tab[i][COLPERS]+BAS;			
-		}
-	}
-		
-	//but en bas
-	if(dify > 0){
-		if(abs(dify) <= (n/2)){
-			depy = tab[i][COLPERS]+BAS;
-			}
-		else{
-			depy = tab[i][COLPERS]+HAUT;
-		}
-	}
-	depx = rebouclement(depx,n);
-	depy = rebouclement(depy,n);
-	
-	//test si case libre
+static int allerVersBut(int tab[][NB_INFO_PERS], int nbp, int depx, int depy, int i){
+	int dep=0;
 	if (!libre(tab,nbp,depx,depy)){
 		tab[i][LIGNEPERS] =depx;
 		tab[i][COLPERS] =depy;
@@ -359,187 +343,76 @@ static void move(int n, int tab[][NB_INFO_PERS], int nbp, int i){
 		tab[i][COLPERS] =depy;
 		dep=1;
 		}
-		
-	/*if(tab[i][LIGNEPERS] < POSMIN || tab[i][LIGNEPERS] == n){
-		(tab[i][LIGNEPERS] < POSMIN )? (tab[i][LIGNEPERS] = (n+GAUCHE)) : (tab[i][LIGNEPERS] = POSMIN);
-		}
-	if(tab[i][COLPERS] < POSMIN || tab[i][COLPERS] == n){
-		(tab[i][COLPERS] < POSMIN )? (tab[i][COLPERS] = (n+HAUT)) : (tab[i][COLPERS] = POSMIN);
-		}*/
-			
-	//si elle peut pas aller vers l'objectif
-	while(dep != 1){
-		switch(k){
-			case 0:
-				if(!libre(tab,nbp,tab[i][LIGNEPERS]+GAUCHE,tab[i][COLPERS]+HAUT)){
-					tab[i][LIGNEPERS]--;
-					tab[i][COLPERS]--;
-					dep=1;
-					break;
+	return dep;
+	}
+
+static int bloquage(int tab[][NB_INFO_PERS], int posx, int posy, int nbp, int i, int n){
+	int depx;
+	int depy;
+	int dep = 0;
+	for (int k=GAUCHE;k<=DROITE;k++){
+		for (int j=HAUT;j<=BAS;j++){
+			depx = posx + k;
+			depy = posy + j;
+			depx = rebouclement(depx,n);
+			depy = rebouclement(depy,n);
+			if(!(k == j && k == 0)){
+				if (!libre(tab, nbp, depx, depy)){
+					tab[i][LIGNEPERS] = depx;
+					tab[i][COLPERS] = depy;
+					dep = 1;
 					}
-			case 1:
-				if(!libre(tab,nbp,tab[i][LIGNEPERS],tab[i][COLPERS]+HAUT)){
-					tab[i][COLPERS]--;
-					dep=1;
-					break;
-					}
-			case 2:
-				if(!libre(tab,nbp,tab[i][LIGNEPERS]+DROITE,tab[i][COLPERS]+HAUT)){
-					tab[i][LIGNEPERS]++;
-					tab[i][COLPERS]--;
-					dep=1;
-					break;
-					}
-			case 3:
-				if(!libre(tab,nbp,tab[i][LIGNEPERS]+GAUCHE,tab[i][COLPERS])){
-					tab[i][LIGNEPERS]--;
-					dep=1;
-					break;
-					}
-			case 4:
-				if(!libre(tab,nbp,tab[i][LIGNEPERS]+DROITE,tab[i][COLPERS])){
-					tab[i][LIGNEPERS]++;
-					dep=1;
-					break;
-					}
-			case 5:
-				if(!libre(tab,nbp,tab[i][LIGNEPERS]+GAUCHE,tab[i][COLPERS]+BAS)){
-					tab[i][LIGNEPERS]--;
-					tab[i][COLPERS]++;
-					dep=1;
-					break;
-					}
-			case 6:
-				if(!libre(tab,nbp,tab[i][LIGNEPERS],tab[i][COLPERS]+BAS)){
-					tab[i][COLPERS]++;
-					dep=1;
-					break;
-					}
-			case 7:
-				if(!libre(tab,nbp,tab[i][LIGNEPERS]+DROITE,tab[i][COLPERS]+BAS)){
-					tab[i][LIGNEPERS]++;
-					tab[i][COLPERS]++;
-					dep=1;
-					break;
-					}
-			case 8:
-				dep=1;
-				nouvBut(n,tab,i);
-				break;
+				}
 			}
-		k++;
 		}
-	if(tab[i][LIGNEPERS] < POSMIN || tab[i][LIGNEPERS] == n){
-		(tab[i][LIGNEPERS] < POSMIN )? (tab[i][LIGNEPERS] = (n+GAUCHE)) : (tab[i][LIGNEPERS] = POSMIN);
+	return dep;
+	}
+
+static void move(int n, int tab[][NB_INFO_PERS], int nbp, int i){
+	int depx = tab[i][LIGNEPERS];
+	int depy = tab[i][COLPERS];
+	int dep;
+	if (tab[i][LIGNEPERS] == tab[i][LIGNEBUT] && tab[i][COLPERS] == tab[i][COLBUT]){
+		nouvBut(n,tab,i);
 		}
-	if(tab[i][COLPERS] < POSMIN || tab[i][COLPERS] == n){
-		(tab[i][COLPERS] < POSMIN )? (tab[i][COLPERS] = (n+HAUT)) : (tab[i][COLPERS] = POSMIN);
+	depx = depx + shortest(depx, tab[i][LIGNEBUT], n);
+	depy = depy + shortest(depy, tab[i][COLBUT], n);
+	depx = rebouclement(depx,n);
+	depy = rebouclement(depy,n);
+	dep = allerVersBut(tab, nbp, depx, depy, i);
+	if (!dep){
+		dep = bloquage(tab, depx, depy, nbp, i, n);
+		}
+	if (!dep){
+		nouvBut(n, tab, i);
 		}
 }
 
 static void contamine(int tab[][NB_INFO_PERS], int i, int nbp, int n){
-	int k=0;
-	int posx, posy, val;
-	if (tab[i][ETAT] == CONT){
-		while(k<NB_CASE){
-			posx = tab[i][LIGNEPERS];
-			posy = tab[i][COLPERS];
-		switch(k){
-			case 0:
-				posx = posx+GAUCHE;
-				posy = posy+HAUT;
-				break;
-			case 1:
-				posy = posy+HAUT;
-				break;
-			case 2:
-				posx = posx+DROITE;
-				posy = posy+HAUT;
-				break;
-			case 3:
-				posx = posx+GAUCHE;
-				break;
-			case 4:
-				posx = posx+DROITE;
-				break;
-			case 5:
-				posx = posx+GAUCHE;
-				posy = posy+BAS;
-				break;
-			case 6:
-				posy = posy+BAS;
-				break;
-			case 7:
-				posx = posx+DROITE;
-				posy = posy+BAS;
-				break;
-			}
-		posx = rebouclement(posx,n);
-		posy = rebouclement(posy,n);
-		val=libre(tab,nbp,posx,posy);
-		if(val){
-			if(tab[val-DECALAGE][ETAT] == NORMAL){
-				tab[val-DECALAGE][ETAT] = INCUB;
-				printf("pers %d cont par %d\n", i, val-1);
+	int etat = tab[i][ETAT];
+	int posx = tab[i][LIGNEPERS];
+	int posy = tab[i][COLPERS];
+	int indice, depx, depy;
+	for (int k=GAUCHE;k<=DROITE;k++){
+		for (int j=HAUT;j<=BAS;j++){
+			depx = posx + k;
+			depy = posy + j;
+			depx = rebouclement(depx,n);
+			depy = rebouclement(depy,n);
+			if (etat == CONT){
+				indice = libre(tab, nbp, depx, depy)-DECALAGE;
+				if (tab[indice][ETAT] == NORMAL){
+					tab[indice][ETAT] = INCUB;
+					}
 				}
-			}	
-		k++;
-		}
-	}
-	
-	if (tab[i][ETAT] == NORMAL){
-		while(k<NB_CASE){
-			posx = tab[i][LIGNEPERS];
-			posy = tab[i][COLPERS];
-		switch(k){
-			case 0:
-				posx = posx+GAUCHE;
-				posy = posy+HAUT;
-				break;
-			case 1:
-				posy = posy+HAUT;
-				break;
-			case 2:
-				posx = posx+DROITE;
-				posy = posy+HAUT;
-				break;
-			case 3:
-				posx = posx+GAUCHE;
-				break;
-			case 4:
-				posx = posx+DROITE;
-				break;
-			case 5:
-				posx = posx+GAUCHE;
-				posy = posy+BAS;
-				break;
-			case 6:
-				posy = posy+BAS;
-				break;
-			case 7:
-				posx = posx+DROITE;
-				posy = posy+BAS;
-				break;
-			}
-		posx = rebouclement(posx,n);
-		posy = rebouclement(posy,n);
-		val=libre(tab,nbp,posx,posy);
-		if(val){
-			if(tab[val-DECALAGE][ETAT] == CONT){
-				tab[i][ETAT] = INCUB;
-				printf("pers %d cont par %d\n", i, val-1);
+			if (etat == NORMAL){
+				indice = libre(tab, nbp, depx, depy)-DECALAGE;
+				if (tab[indice][ETAT] == CONT){
+					tab[i][ETAT] = INCUB;
+					}
 				}
-			}	
-		k++;
+			}
 		}
-	}	
-	}
-
-static void print(int tab[], int a){
-	for(int i=0;i<a;i++){
-		printf("%d ",tab[i]);
-		}
-	printf("\n");
 	}
 	
 static void mergemoi(int tab[],int liun[], int lide[], int tun, int tde){
@@ -594,16 +467,24 @@ static int simul(int tab[][NB_INFO_PERS], int n, int nbp, int afG, int nbvacc){
 	int nbCycle=0;
 	int nbCont=1;
 	int i;
-	for (i=0;i<nbvacc;i++){
-		tab[nbp-i][ETAT]= VACC;
+	for (i=1;i<=nbvacc;i++){
+		tab[i][ETAT]= VACC;
 		}
+	if (afG){
+			grille(tab, n, nbp);
+			}
+	contamine(tab, 0, nbp, n);
+	for (i=0;i<nbp;i++){
+			if (tab[i][ETAT] == INCUB){
+				tab[i][ETAT] = CONT;
+				nbCont++;
+				}
+			}
 	while(nbCycle < NB_MAX_CYCLE && nbCont < nbp-nbvacc){
 		for (i=0;i<nbp;i++){
 			if (tab[i][ETAT] == INCUB){
 				tab[i][ETAT] = CONT;
 				nbCont++;
-				printf("nbCont %d lim=%d", nbCont, nbp-nbvacc);
-				print(tab[i], nbp);
 				}
 			}
 		for(i=0;i<nbp;i++){
@@ -616,11 +497,40 @@ static int simul(int tab[][NB_INFO_PERS], int n, int nbp, int afG, int nbvacc){
 		nbCycle++;
 		}
 	for(i=0;i<nbp;i++){
-		print(tab[i],NB_INFO_PERS);
 		}
 	return nbCycle;
 	}
 
+static void regroupe(int tab[][NB_INFO_PERS], int tailleRes, int n, int nbp, int afG, int nbSim){
+	int nbpSim, nbpVacc, k, l, m;
+	float compte;
+	int tabde[nbp][NB_INFO_PERS];
+	int tabForMerg[nbSim];
+	for (nbpSim=nbp; nbpSim>=NB_MIN_PERS; nbpSim--){
+		for(nbpVacc=0; nbpVacc<nbpSim-1; nbpVacc++){
+			for(k=0; k<nbSim; k++){
+				for (l=0; l<nbpSim; l++){
+					for(m=0; m<NB_INFO_PERS; m++){
+						tabde[l][m] = tab[l][m];
+						}
+					}
+				
+				tabForMerg[k] = simul(tabde, n, nbpSim, afG, nbpVacc); 
+				}
+			msort(tabForMerg, nbSim);
+			if (nbSim%2 == 0){
+				compte = ((float)tabForMerg[nbSim/2] + (float)tabForMerg[(nbSim/2)-1])/2;
+				}
+			else{
+				compte = tabForMerg[nbSim/2];
+				}
+			float tauxVacc = (float)nbpVacc/nbpSim;
+			float densitePop = nbpSim/(float)(n*n);
+			printf("%f %f %.1f\n", densitePop, tauxVacc, compte);
+			}
+		printf("\n");
+		}
+	}
 
 // *******************************************************************
 
